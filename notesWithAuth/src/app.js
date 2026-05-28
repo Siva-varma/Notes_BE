1059981573427f5122fc8052c4402e660052cb34
siva <sivavarma1999@gmail.com>
2026-05-28 09:53:48 +0530

    Add error handling middleware and set up Express server with initial routes

--- notesWithAuth/src/app.js
+++ notesWithAuth/src/app.js
@@ -1,8 +1,21 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
+import cookiesParser from 'cookie-parser';
+import errorMiddleware from './middlewares/errorMiddleware.js';
 
 const app = express();
+app.use(express.json());
+app.use(cookiesParser());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Notes API!');
+});
+
+
+
+
+app.use(errorMiddleware)
 
 
 
