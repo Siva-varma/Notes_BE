875fd2e3d4555bcda129b0bd87bdac0829b0506c
siva <sivavarma1999@gmail.com>
2026-05-28 09:58:35 +0530

    Add user model with schema for name, email, and password

---
+++ notesWithAuth/src/models/user.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: [true, "Email must be unique"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+  },
+  { timestamps: true },
+);
+
+const userModel = mongoose.model("User", userSchema);
+
+export default userModel;
